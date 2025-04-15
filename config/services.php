00bb388e97a6e70035fa53e88807936d50294d63
Abdellah Ramadan <ramadanabdel24@gmail.com>
2025-04-15 08:09:06 +0100

    complete timestamped

--- config/services.php
+++ config/services.php
@@ -1,8 +1,17 @@
 <?php
 
+use Rami\EntityKitBundle\EventListener\TimeStamped\TimeStampedListener;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $container) {
-    $services = $container->services();
+    $services = $container->services()
+        ->defaults()
+        ->autowire()
+        ->autoconfigure();
 
+    $services
+        ->set(TimeStampedListener::class)
+        ->tag('doctrine.event_listener', ['event' => 'prePersist'])
+        ->tag('doctrine.event_listener', ['event' => 'preUpdate'])
+    ;
 };
